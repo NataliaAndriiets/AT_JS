c843e251ad92d53edd35498cfa6436c3f5d3db2a
NataliaAndriiets <84592689+NataliaAndriiets@users.noreply.github.com>
2025-01-21 16:19:21 +0000

    Update JsBasics/Conditions_and_loops/student_grades.js
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- JsBasics/Conditions_and_loops/student_grades.js
+++ JsBasics/Conditions_and_loops/student_grades.js
@@ -4,5 +4,5 @@ if (averagegrade < 60) { console.log("Bed"); }
 else if (averagegrade > 60 && averagegrade <= 70) { console.log("Not bed"); }
 else if (averagegrade <= 80) { console.log("Good"); }
 else if ( averagegrade <= 90) { console.log("Very Good"); }
-else if (averagegrade > 90 && averagegrade <= 100) { console.log("Excellent"); }
+else if (averagegrade <= 100) { console.log("Excellent"); }
 
