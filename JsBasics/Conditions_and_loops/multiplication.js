8255e8b3f0b48d27d77ceb11a43257cdde3b2dcb
NataliaAndriiets <84592689+NataliaAndriiets@users.noreply.github.com>
2025-01-21 16:29:19 +0000

    Update JsBasics/Conditions_and_loops/multiplication.js
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- JsBasics/Conditions_and_loops/multiplication.js
+++ JsBasics/Conditions_and_loops/multiplication.js
@@ -7,6 +7,9 @@ for (let i = 0; i <= 10; i++) { console.log(i * 7) }
 //2) while
 
 let count = 0;
-while (count <= 10) { console.log(count*7); count++ }
+while (count <= 10) { 
+    console.log(count*7);
+    count++
+}
 
 
