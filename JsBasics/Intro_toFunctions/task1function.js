178302c599411a39c4e7e8fb165863a0c7cb80ec
Natalia Andriiets <nandriiets@gmail.com>
2025-01-24 10:34:56 +0000

    Intro to functions

---
+++ JsBasics/Intro_toFunctions/task1function.js
@@ -0,0 +1,35 @@
+﻿//Створіть функцію яка приймає два параметри: width і height.
+
+//Усередині функції обчисліть площу прямокутника, перемноживши width на height
+//та поверніть результат з функції.
+
+//Викличте вашу функцію з аргументами(наприклад 5 і 10 і виведіть результат(площу прямокутника) в консоль.
+
+//Реалізуйте функцію трьома способами(function declaration, function expression, arrow function)
+
+
+//1) function declaration
+function calculateSquaire(width, height) {
+const squaire = width * height;
+return squaire
+}
+const result = calculateSquaire(4, 8)
+
+console.log(result);
+
+
+//2) function Expression
+
+const calculateSquare = function(width, height)
+{ const square = width * height;
+return square
+}
+
+console.log(calculateSquare(12, 18));
+
+//3) Arrow function
+
+const calculateSquare = (width, height) => width * height;
+
+const result = calculateSquare(89, 78)
+console.log(result);
---
+++ JsBasics/Intro_toFunctions/task3function.js
@@ -0,0 +1,19 @@
+﻿//Завдання 3
+
+
+
+function checkOrder(available, ordered) {
+
+    if (available <= ordered) {
+        console.log("Your order is too large,we don’t have enough goods.");
+    }
+    else if (ordered = 0) {
+        console.log("Your order is empty");
+    }
+    else if (available >= ordered) {
+        console.log("Your order is accepted");
+    }
+}
+
+checkOrder(7,0)
+checkOrder(7,10)
