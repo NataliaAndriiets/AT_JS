1166939ee089b2bfc5a7b1f2fa3bdac627823c86
Natalia Andriiets <nandriiets@gmail.com>
2025-01-28 16:25:04 +0000

    Functions and their patterns

---
+++ JsBasics/Advances_function_usage/task1.js
@@ -0,0 +1,22 @@
+﻿
+
+const isOdd = (n) => n % 2 !== 0;
+  const isEven = (n) => n % 2 === 0;
+
+  const callback1 = () => {
+    console.log("The number is an odd number.");
+  };
+  const callback2 = () => {
+    console.log("The number is an even number.");
+  };
+
+  const handleNum = (num, callback1, callback2) => {
+    if (isOdd(num)) {
+      callback1();
+    } else if (isEven(num)) {
+      callback2();
+    }
+  };
+
+  handleNum(4, callback1, callback2);
+  handleNum(7, callback1, callback2);
---
+++ JsBasics/Advances_function_usage/task2.js
@@ -0,0 +1,5 @@
+﻿let practice = (num) =>  {
+console.log(num);
+if (num>0) practice(num-1);
+} 
+practice(5);
