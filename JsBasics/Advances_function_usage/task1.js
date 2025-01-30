799891cae855baa75237d4685cc7c449d7b95ce2
Natalia Andriiets <nandriiets@gmail.com>
2025-01-30 17:46:33 +0000

    Fixed 1 task, added 3 task

--- JsBasics/Advances_function_usage/task1.js
+++ JsBasics/Advances_function_usage/task1.js
@@ -1,22 +1,21 @@
 ﻿
 
 const isOdd = (n) => n % 2 !== 0;
-  const isEven = (n) => n % 2 === 0;
 
-  const callback1 = () => {
-    console.log("The number is an odd number.");
-  };
-  const callback2 = () => {
-    console.log("The number is an even number.");
-  };
+const callback1 = () => {
+  console.log("The number is an odd number.");
+};
+const callback2 = () => {
+  console.log("The number is an even number.");
+};
 
-  const handleNum = (num, callback1, callback2) => {
-    if (isOdd(num)) {
-      callback1();
-    } else if (isEven(num)) {
-      callback2();
-    }
-  };
+const handleNum = (num, callback1, callback2) => {
+  if (isOdd(num)) {
+    callback1();
+  } else {
+    callback2();
+  }
+};
 
-  handleNum(4, callback1, callback2);
-  handleNum(7, callback1, callback2);
+ handleNum(4, callback1, callback2);
+ handleNum(7, callback1, callback2);
---
+++ JsBasics/Advances_function_usage/task3.js
@@ -0,0 +1,26 @@
+let divide = (numerator, denominator) => {
+    if (denominator === 0 || typeof numerator !== "number" || typeof denominator !== "number") {
+        return "some agrument is not Number";
+    } else {
+        return numerator / denominator;
+    }
+}
+
+try {
+    divide(15, 5);
+} catch (error) {
+} finally {
+    console.log("work is done");
+}
+try {
+    divide(15, 0);
+} catch (error) {
+} finally {
+    console.log("work is done");
+}
+try {
+    divide('15', 10);
+} catch (error) {
+} finally {
+    console.log("work is done");
+}
