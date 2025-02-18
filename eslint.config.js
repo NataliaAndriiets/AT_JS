5cbc0d8b4f50b84ba1c2e0c34d2c1b9c59a92674
Natalia Andriiets <nandriiets@gmail.com>
2025-02-18 16:54:41 +0000

    add husky 2

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,50 @@
+import js from "@eslint/js";
 
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
+    js.configs.recommended,
+
     {
-        files: ["*/.{js,mjs,cjs,vue}"],
-        languageOptions: {
-            globals: { ...globals.browser, ...globals.node }
-        },
         rules: {
-            // Indentation: 2 spaces, including for switch cases
-            "indent": [
-                "error",
-                2,
-                { "SwitchCase": 1 }
-            ],
-            // Double quotes for strings, but allow template literals
-            "quotes": [
-                "warn",
-                "double",
-                { "avoidEscape": true, "allowTemplateLiterals": true }
-            ],
-            // Always require parentheses for arrow functions
-            "arrow-parens": [
-                "error",
-                "always"
-            ]
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
         }
-    },
-    pluginJs.configs.recommended,
-    ...pluginVue.configs["flat/essential"],
+    }
 ];
+
+
+
+
+//import globals from "globals";
+//import pluginJs from "@eslint/js";
+//import pluginVue from "eslint-plugin-vue";
+
+///** @type {import('eslint').Linter.FlatConfig[]} */
+//export default [
+//    {
+//        files: ["*/.{js,mjs,cjs,vue}"],
+//        languageOptions: {
+//            globals: { ...globals.browser, ...globals.node }
+//        },
+//        rules: {
+//            // Indentation: 2 spaces, including for switch cases
+//            "indent": [
+//                "error",
+//                2,
+//                { "SwitchCase": 1 }
+//            ],
+//            // Double quotes for strings, but allow template literals
+//            "quotes": [
+//                "warn",
+//                "double",
+//                { "avoidEscape": true, "allowTemplateLiterals": true }
+//            ],
+//            // Always require parentheses for arrow functions
+//            "arrow-parens": [
+//                "error",
+//                "always"
+//            ]
+//        }
+//    },
+//    pluginJs.configs.recommended,
+//    ...pluginVue.configs["flat/essential"],
+//];
