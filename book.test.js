e1f6fcce8cca3668e9da3becb879723c8910dcad
NataliaAndriiets <84592689+NataliaAndriiets@users.noreply.github.com>
2025-02-24 22:31:45 +0000

    Update book.test.js

--- book.test.js
+++ book.test.js
@@ -23,6 +23,7 @@ test("Get all todos list", async () => {
 
 
 //test get 2
+
 test("Get all todos list", async () => {
     const response = await axios.get(
         'https://jsonplaceholder.typicode.com/photos',
@@ -37,26 +38,6 @@ test("Get all todos list", async () => {
 
 
 
-//test("Create new Post", async () => {
-//    var response = await axios.post(
-//        'https://jsonplaceholder.typicode.com/posts',
-//        {
-//            title: 'foo',
-//            body: 'bar',
-//            userId: 1,
-//        },
-//        {
-//            headers: {
-//                'Content-type': 'application/json; charset=UTF-8',
-//            },
-//        }
-//    );
-
-//    console.log(response.data)
-//    expect(response.status).toEqual(201);
-//    expect(response.data.title).toContain("foo");
-
-//});
 
 
 
