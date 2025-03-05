c481e9eea5696dab4962a8a1f2cc342e9a4c062a
Natalia Andriiets <nandriiets@gmail.com>
2025-03-05 21:58:33 +0000

    error

---
+++ task1.test.js
@@ -0,0 +1,29 @@
+﻿
+
+
+const axios = require('axios');
+
+
+test('test link error', async () => {
+    let response = await axios.post(`https://jjsonplaceholder.typicode.com/comments`,
+        {
+            "postId": 1,
+            "name": "id labore ex et quam laborum",
+            "email": "Eliseo@gardner.biz",
+            "body": "555555555555555"
+        },
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(response => {
+            if (response.status === 200) {
+            }
+            else {
+                throw new Error("The link is not correct");
+            }
+            return 'Valid input';
+        }).catch(error => {
+            console.log(error)
+        });
+});
---
+++ task3.test.js
@@ -0,0 +1,39 @@
+﻿//
+
+const getFirstUserSurname = require('./users');
+
+const axios = require('axios');
+
+jest.mock('axios');
+
+//test1
+test('returns the Surname of the first user', async () => {
+    axios.get.mockResolvedValue({
+        data: [
+            {
+                userId: 1,
+                id: 1,
+                surname: 'Miles'
+            },
+            {
+                userId: 1,
+                id: 2,
+                surname: 'Sequel'
+            }
+        ]
+    });
+
+    const surname = await getFirstUserSurname();
+    expect(surname).toEqual('Miles');
+});
+
+
+//test2
+test('Returns the error', async () => {
+    let response = await axios.post.mockResolvedValue(new Error("Surname Error"))
+
+    expect(await response())
+});
+
+    
+    
